d614956efb0137387859df6af185fbd68407fc09
VishSeen <vishseenarain@gmail.com>
2026-03-04 01:20:12 +0400

    rebuild: fix admin API URL

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ cacheDir: '/Users/vishseen/Developer/projects/nxt-vish-studio-v1/tina/__generated__/.cache/1772572543200', url: 'https://content.tinajs.io/2.1/content/94cff29e-b158-496c-b456-9850440a0fb9/github/main', token: 'effdd5b419c83677e081c08c90c3a1dee3d7b399', queries,  });
+export const client = createClient({ cacheDir: '/Users/vishseen/Developer/projects/nxt-vish-studio-v1/tina/__generated__/.cache/1772572795062', url: 'https://content.tinajs.io/2.1/content/94cff29e-b158-496c-b456-9850440a0fb9/github/main', token: 'effdd5b419c83677e081c08c90c3a1dee3d7b399', queries,  });
 export default client;
   
